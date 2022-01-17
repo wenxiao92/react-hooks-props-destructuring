f68927c7fc0a9f5db311ca4d3e69806789d69289
Wen Xiao <wennstyle@gmail.com>
2022-01-17 16:14:56 -0500

    commit tree 0361b69a76bf636e6d17cd8e510ad44de0014fb7

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 function MovieCard({
-  title,
+  titleTest,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
 }) {
   return (
     <div className="movie-card">
-      <img src={posterSrc} alt={title} />
-      <h2>{title}</h2>
+      <img src={posterSrc} alt={titleTest} />
+      <h2>{titleTest}</h2>
       <small>{genres.join(", ")}</small>
     </div>
   );
